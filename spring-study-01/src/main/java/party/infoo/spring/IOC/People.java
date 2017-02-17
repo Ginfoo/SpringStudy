c04cfb1542e3d6f6e640a18673503ad005a41f38
infoo <changzhou1026@126.com>
2017-02-18 02:38:38 +0800

    testSpringIOC

---
+++ spring-study-01/src/main/java/party/infoo/spring/IOC/People.java
@@ -0,0 +1,16 @@
+package party.infoo.spring.IOC;
+
+/**
+ * Created by infoo on 2017/2/18.
+ */
+public class People {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ spring-study-01/src/main/java/party/infoo/spring/IOC/Work.java
@@ -0,0 +1,11 @@
+package party.infoo.spring.IOC;
+
+/**
+ * Created by infoo on 2017/2/18.
+ */
+public class Work {
+    private People people;
+    public void doWork(){
+        System.out.println(people.getName()+" doWork!");
+    }
+}
